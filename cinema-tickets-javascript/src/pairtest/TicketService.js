2df13dea1a38bea4c9cd97280d457d4813f50bbe
vijayarajan Manoharan <vijayarajanbtech@gmail.com>
2026-07-16 10:00:23 +0100

    build: write validation for account id

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -4,6 +4,13 @@ import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
 export default class TicketService {
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
+    this.validateAccountId(accountId);
    return "SUCCESS";
   }
+
+  validateAccountId(accountId) {
+    if (accountId === 0) {
+    throw new InvalidPurchaseException('Account ID must be a valid positive integer.');
+    }
+ }
 }
